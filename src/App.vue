80b9878a67acf03930bd388805a6f1d415031d35
Afonso Pinto <105606345+AfonsoPint0@users.noreply.github.com>
2022-11-08 16:25:21 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="search" />
+    <div v-for="post in filteredPosts" :key="post.id">
+      <post :post="post"></post>
+    </div>
+  </div>
+</template>
+
+<script>
+import Post from "./components/Post";
+export default {
+  name: "App",
+  components: { Post },
+  data() {
+    return {
+      search: "",
+      posts: [
+        {
+          id: 0,
+          title: "0",
+          body: "Box",
+        },
+        {
+          id: 1,
+          title: "1",
+          body: "Rope",
+        },
+        {
+          id: 2,
+          title: "2",
+          body: "Plank",
+        },
+      ],
+    };
+  },
+  computed: {
+    filteredPosts() {
+      return this.posts.filter((post) =>
+        post.body.toLowerCase().includes(this.search.toLowerCase())
+      );
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,16 @@
+<style>
+.grid {
+  display: grid;
+}
+</style>
+<template>
+  <article>
+    <p>{{ post.title }} - {{ post.body }}</p>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ["post"],
+};
+</script>
